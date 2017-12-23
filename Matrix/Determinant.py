70f1b92ab77fd140d7dad0d8432a22555bad0920
park young jae <jolie0215@gmail.com>
2017-12-23 13:29:04 +0900

    Create Determinant.py
    행렬식을 구하는 코드

---
+++ Matrix/Determinant.py
@@ -0,0 +1,47 @@
+import copy
+# 리스트를 복사할때 내장된 list(), copy()를 이용하면 어인일인지 원본도 훼손된다.
+# 따라서 반드시 copy모듈을 이용해 깊은 복사를 해주어야 한다.
+
+MAT = []
+D = 0
+def Matrix(n):
+    for i in range(n):
+        str = input()   
+        MAT.append(str.split(' '))
+
+
+def Determinant(n, mat):
+    global D
+    mat_copy = copy.deepcopy(mat)
+    value = []
+    mat_tmp = []
+    if n>=3:
+        for i in range(n):
+            mat_tmp.clear()
+            mat_tmp = copy.deepcopy(mat_copy)
+            tmp = int(mat_tmp[0][i])
+            if i%2 == 0:
+                # 행 삭제
+                del mat_tmp[0]
+                # 열 삭제 
+                for j in range(0, n-1):
+                    del mat_tmp[j][i]
+                value.append((tmp*Determinant(n-1, mat_tmp)))
+            else:
+                # 행 삭제
+                del mat_tmp[0]
+                # 열 삭제 
+                for j in range(0, n-1):
+                    del mat_tmp[j][i]
+                value.append(((-1)*tmp*Determinant(n-1, mat_tmp)))
+    else:
+        return int(int(mat_copy[0][0])*int(mat_copy[1][1])-int(mat_copy[0][1])*int(mat_copy[1][0]))
+    D = sum(value, 0)
+    return D
+    
+
+num = int(input())
+Matrix(num)
+print(MAT)
+Determinant(num, MAT)
+print(D)
