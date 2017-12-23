df3e2baab3bcb78f88e38f64eea77b9d3df8674e
park young jae <jolie0215@gmail.com>
2017-12-23 13:38:10 +0900

    Update Determinant.py

--- Matrix/Determinant.py
+++ Matrix/Determinant.py
@@ -6,7 +6,14 @@ def Matrix(n):
     for i in range(n):
         str = input()   
         MAT.append(str.split(' '))
-
+#  입력 예시
+#
+#  1 2 3 4
+#  5 6 7 8
+#  4 3 2 1
+#  8 7 6 5
+#
+# 행의 원소는 모두 한줄이고 구분은 띄어쓰기를 이용한다.
 
 def Determinant(n, mat):
     global D
