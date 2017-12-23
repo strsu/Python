7e26055b8440cd4b5b1d146bc1e1f4daefaac65f
park young jae <jolie0215@gmail.com>
2017-12-23 13:33:01 +0900

    Update Determinant.py

--- Matrix/Determinant.py
+++ Matrix/Determinant.py
@@ -2,8 +2,6 @@ import copy
 # 리스트를 복사할때 내장된 list(), copy()를 이용하면 어인일인지 원본도 훼손된다.
 # 따라서 반드시 copy모듈을 이용해 깊은 복사를 해주어야 한다.
 
-MAT = []
-D = 0
 def Matrix(n):
     for i in range(n):
         str = input()   
@@ -20,14 +18,14 @@ def Determinant(n, mat):
             mat_tmp.clear()
             mat_tmp = copy.deepcopy(mat_copy)
             tmp = int(mat_tmp[0][i])
-            if i%2 == 0:
+            if i%2 == 0: # 짝순열
                 # 행 삭제
                 del mat_tmp[0]
                 # 열 삭제 
                 for j in range(0, n-1):
                     del mat_tmp[j][i]
                 value.append((tmp*Determinant(n-1, mat_tmp)))
-            else:
+            else: # 홀순열
                 # 행 삭제
                 del mat_tmp[0]
                 # 열 삭제 
@@ -38,10 +36,14 @@ def Determinant(n, mat):
         return int(int(mat_copy[0][0])*int(mat_copy[1][1])-int(mat_copy[0][1])*int(mat_copy[1][0]))
     D = sum(value, 0)
     return D
-    
 
-num = int(input())
-Matrix(num)
-print(MAT)
-Determinant(num, MAT)
-print(D)
+# global 
+MAT = []
+D = 0
+try:
+    num = int(input())
+    Matrix(num)
+    print(MAT)
+    Determinant(num, MAT)
+finish:
+    print(D)
